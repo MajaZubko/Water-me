7450e1565c63d4957eadae5976913016b15a76a0
MajaZubko <majamzubko@gmail.com>
2020-12-12 19:32:48 +0100

    Change home page

--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { FormattedMessage, useIntl } from 'react-intl';
 
-import { LanguageSwitcher } from '../../shared/components/languageSwitcher';
 import { H1 } from '../../theme/typography';
-import { Users } from '../../shared/components/users';
-import { Container, Logo } from './home.styles';
+import { Container } from './home.styles';
 
 export const Home = () => {
   const intl = useIntl();
@@ -18,16 +16,9 @@ export const Home = () => {
           description: 'Home / page title',
         })}
       />
-
       <H1>
-        <FormattedMessage defaultMessage="Hello world!" description="Home / title" />
+        <FormattedMessage defaultMessage="Water me - the app" description="Home / title" />
       </H1>
-
-      <Logo />
-
-      <LanguageSwitcher />
-
-      <Users />
     </Container>
   );
 };
