83b15d9d24c86ca962ac32f9041cae2e5496510d
MajaZubko <majamzubko@gmail.com>
2020-12-30 11:18:18 +0100

    Read and write plants from localStorage

--- src/modules/plants/plants.sagas.ts
+++ src/modules/plants/plants.sagas.ts
@@ -1,18 +1,28 @@
-import { all, put, takeLatest } from 'redux-saga/effects';
+import { all, put, select, takeLatest } from 'redux-saga/effects';
+import { isEmpty } from 'lodash';
 
 import { reportError } from '../../shared/utils/reportError';
-import { plants as plantsFixture } from '../../mocks/fixtures/plants.json';
 import * as plantsActions from './plants.actions';
+import { selectPlants } from './plants.selectors';
 
 function* fetchPlants() {
   try {
-    yield put(plantsActions.fetchPlantsSuccess(plantsFixture));
+    const storagePlants = localStorage.getItem('plants');
+    const plants = JSON.parse(storagePlants || '');
+    yield put(plantsActions.fetchPlantsSuccess(plants));
   } catch (error) {
     reportError(error);
     yield put(plantsActions.fetchPlantsFailure(error));
   }
 }
 
+function* savePlants() {
+  const plants = yield select(selectPlants);
+  if (!isEmpty(plants)) {
+    localStorage.setItem('plants', JSON.stringify(plants));
+  }
+}
+
 export function* watchPlants() {
-  yield all([takeLatest(plantsActions.fetchPlants, fetchPlants)]);
+  yield all([takeLatest(plantsActions.fetchPlants, fetchPlants), takeLatest(plantsActions.addPlant, savePlants)]);
 }
